e13b777863a5047055a09eadda5b576e9c17dc4d
razwanizmi <razwan.izmi@gmail.com>
2016-04-18 18:01:20 +0800

    updated procfile

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,6 @@
 class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:show, :edit, :update, :destroy]
+  before_action :require_login
 
   # GET /reservations
   def index
