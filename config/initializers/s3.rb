0fcdd69c48bef5aaf300b9414c3bec4a0a709f8e
razwanizmi <razwan.izmi@gmail.com>
2016-04-18 17:41:21 +0800

    updated security credentials

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV["AMAZON ACCESS KEY"],
-      :aws_secret_access_key  => ENV["AMAZON SECRET KEY"],
+      :aws_access_key_id      => ENV["AMAZON_ACCESS_KEY"],
+      :aws_secret_access_key  => ENV["AMAZON_SECRET_KEY"],
       :region                 => 'ap-southeast-1' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "razbnb"
